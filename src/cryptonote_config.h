5ab531c30309075b2471252cf8d95fee5ce9b4d9
kolm <hoko@tkmc.jk>
2026-04-07 17:00:14 +0000

    update

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -244,10 +244,10 @@ namespace config
   boost::uuids::uuid const NETWORK_ID_V16 = { {
       0x26 ,0x8F, 0x7D, 0xF3 , 0x8A, 0x95 , 0x4E, 0x1B, 0x9C, 0x22, 0x9A, 0xB1, 0x5F, 0x4D, 0x30, 0x40
     } };
-  std::string const GENESIS_TX_V16 = "013c01ff0001ffffffffffff0302df5d56da0c7d643ddd1ce61901c7bdc5fb1738bfe39fbe69c28a3a7032729c0f2101168d0c4ca86fb55a4cf6a36d31431be1c53a3bd7411bb24e8832410289fa6f3b";
+  std::string const GENESIS_TX_V16 = "020600000001007d5f00500135e9dc8e7625efca276142250a922c4d5925886744c3098797605e03210077157c0be9953fb472d8e22e54376f25d59da41759f54e0d276123149b07287d0140b3e09edfa4c1cc5b0dd387b4b0038248c7b5be175aa8637803be4e9c4a3e4502";
   boost::uuids::uuid const NETWORK_ID = NETWORK_ID_V16;
   std::string const GENESIS_TX = GENESIS_TX_V16;
-  uint32_t const GENESIS_NONCE = 10000;
+  uint32_t const GENESIS_NONCE = 4009755871;
 
   // Hash domain separators
   const char HASH_KEY_BULLETPROOF_EXPONENT[] = "bulletproof";
